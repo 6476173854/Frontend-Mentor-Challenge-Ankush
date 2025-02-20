7562a1873557bf615867fa1128dd5b019181b5e9
Ankushbansal <144263575+6476173854@users.noreply.github.com>
2025-02-20 15:10:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownToggles = document.querySelectorAll('.nav-item.dropdown');
+
+    dropdownToggles.forEach(function(dropdown) {
+        dropdown.addEventListener('show.bs.dropdown', function () {
+            const openDropdowns = document.querySelectorAll('.dropdown-menu.show');
+            openDropdowns.forEach(function(menu) {
+                if (menu !== dropdown.querySelector('.dropdown-menu')) {
+                    const dropdownInstance = bootstrap.Dropdown.getInstance(menu.closest('.dropdown'));
+                    dropdownInstance.hide();
+                }
+            });
+        });
+    });
+});
